d298f0d1feca2858e0ec26100b537ecca2c6be93
estevaons <estevao_gt@hotmail.com>
2022-06-02 23:34:37 -0300

    atualização auth

--- pages/api/email.js
+++ pages/api/email.js
@@ -5,8 +5,12 @@ export default async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
+      type: 'OAuth2',
       user: process.env.EMAIL,
-      pass: process.env.PASSWORD
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: process.env.GOOGLE_ACCESS_TOKEN,
     }
   });
 
